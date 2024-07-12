d595dfb09cd68655b0f937fffeb1b3995306fa08
Vinh1222001 <nguyentuanvinh1222@gmail.com>
2024-07-12 12:33:28 +0700

    Create Project and define pins

---
+++ sources/Self-Balancing Robot/include/pins_define.hpp
@@ -0,0 +1,17 @@
+#ifndef PIN_DEFINE_HPP
+#define PIN_DEFINE_HPP
+#endif
+
+// Define I2C pins
+#define I2C_SCL 22
+#define I2C_SDA 21
+
+// Define L298 Motor Driver pins
+#define MOTOR_IN1   19
+#define MOTOR_IN2   28
+#define MOTOR_IN3   17
+#define MOTOR_IN4   16
+#define MOTOR_ENA   25
+#define MOTOR_ENB   26
+
+
